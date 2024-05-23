a0c87ea8396774e85faf1e5c0563b1835921fb94
dawcioxd3 <168067151+dawcioxd3@users.noreply.github.com>
2024-05-23 13:07:44 +0200

    wip: add update function

--- main.py
+++ main.py
@@ -29,39 +29,40 @@ def add_user(users: list) -> None:
     :param users: user list
     :return: None
     """""
-    name: str=input("Enter your name: ")
-    surname:str=input("Enter your surname: ")
-    posts:int=int(input("Enter your number of posts: "))
-    location:str=input("Enter your location: ")
-    new_user: dict={'name': name, 'surname': surname, 'posts': posts, 'Location': location}
+    name: str = input("Enter your name: ")
+    surname: str = input("Enter your surname: ")
+    posts: int = int(input("Enter your number of posts: "))
+    location: str = input("Enter your location: ")
+    new_user: dict = {'name': name, 'surname': surname, 'posts': posts, 'Location': location}
     users.append(new_user)
 
     # add_user(data_of_users)
     # read(data_of_users)
-    def delete_user(users: list) -> None:
-    name: str=input("Enter name of user to remowe: ")
-    for user in users:
-        if user['name']==name:
-        users.remove(user)
-        read(data_of_users)
-        read(users)
 
-# delete_user(data_of_users)
-# read(data_of_users)
+
+def delete_user(users: list) -> None:
+    name: str = input("Enter name of user to remowe: ")
+    for user in users:
+        if user['name'] == name:
+            users.remove(user)
+
+
+delete_user(data_of_users)
+read(data_of_users)
 
 def update(users: list) -> None:
-
-    name:str=input("Enter name of user to be modified: ")
+    name: str = input("Enter name of user to be modified: ")
     for user in users:
-        if user ['name']==name:
-            new_name:str=input('Enter new name: ')
-            new_surname:str=input('Enter new surname: ')
-            new_posts:int=int(input('Enter new number of posts: '))
-            new_location:str=input('Enter new location: ')
-            user['name']=new_name
-            user['surname']=new_surname
-            user['posts']=new_posts
-            user['Location']=new_location
+        if user['name'] == name:
+            new_name: str = input('Enter new name: ')
+            new_surname: str = input('Enter new surname: ')
+            new_posts: int = int(input('Enter new number of posts: '))
+            new_location: str = input('Enter new location: ')
+            user['name'] = new_name
+            user['surname'] = new_surname
+            user['posts'] = new_posts
+            user['Location'] = new_location
 
-update(data_of_users)
-read(data_of_users)
+
+# update(data_of_users)
+# read(data_of_users)
