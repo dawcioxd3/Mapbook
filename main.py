9b1956dfc942fa173a344a31b211dbf2302eeb56
dawcioxd3 <168067151+dawcioxd3@users.noreply.github.com>
2024-05-23 12:38:57 +0200

    wip: add delete function

--- main.py
+++ main.py
@@ -8,13 +8,45 @@ data_of_users: list = [
     {'name': 'Sebastian', 'surname': 'Wątroba', 'posts': 12, 'Location': 'Siedlce'},
 ]
 print(f'Witaj {data_of_users[0]['name']}')
-def read(users:list)->None:
+
+
+def read(users: list) -> None:
     ''''
-    this is a function to show users from an list
+    show users from an list
     :param users: a list of users
     :return: None
     '''
     for user in users[1:]:
         print(f"Twój znajomy:  {user['name']},opublikował: {user['posts']}")
 
+
 read(data_of_users)
+
+
+def add_user(users: list) -> None:
+    """
+    add user to a list
+    :param users: user list
+    :return: None
+    """""
+    name: str=input("Enter your name: ")
+    surname:str=input("Enter your surname: ")
+    posts:int=int(input("Enter your number of posts: "))
+    location:str=input("Enter your location: ")
+    new_user: dict={'name': name, 'surname': surname, 'posts': posts, 'Location': location}
+    users.append(new_user)
+
+    # add_user(data_of_users)
+    # read(data_of_users)
+    def delete_user(users: list) -> None:
+    name: str=input("Enter name of user to remowe: ")
+    for user in users:
+        if user['name']==name:
+        users.remove(user)
+        read(data_of_users)
+        read(users)
+
+delete_user(data_of_users)
+read(data_of_users)
+
+
